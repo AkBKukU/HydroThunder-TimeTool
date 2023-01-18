6e2952c674e936e69a7298f4d57764815e0cea01
WizardTim <43670403+WizardTim@users.noreply.github.com>
2023-01-19 01:21:23 +1000

    Update HTchecksumUtilsTest.py

--- HTchecksumUtilsTest.py
+++ HTchecksumUtilsTest.py
@@ -33,7 +33,7 @@ verifyImageHeaders([cmos_image_path], cmos_img_offset)
 
 print()
 
-print("Checksum from image : {}".format(getChecksum('SUM32', cmos_image_path, 0x75BE66F).hex(' ')))
+print("Checksum from image : {}".format(readChecksum(cmos_image_path, 0x75BE66F).hex(' ')))
 
 print()
 
@@ -41,7 +41,7 @@ algorithm_list = ['SUM8', 'SUM16', 'SUM24', 'SUM32']
 checksum_lengths = [1000, 1000, 999, 1000]
 
 for i in range(len(algorithm_list)):
-	checksum, checksum_start_offset, checksum_end_offset, num_sums, last_byte = calculateChecksum(algorithm_list[i], 'little', cmos_image_path, 0x075be85b, checksum_lengths[i])
+	checksum, checksum_start_offset, checksum_end_offset, num_sums, last_byte = calculateChecksum(cmos_image_path, 0x075be85b, checksum_lengths[i], algorithm_list[i], 'little', 0x0)
 	print("{:5s}  {:12s}  {:10s} → {:10s}  {:5d}".format(algorithm_list[i], checksum.hex(' '), checksum_start_offset.to_bytes(4,'big').hex(), checksum_end_offset.to_bytes(4,'big').hex(), num_sums))
 
 
@@ -52,7 +52,7 @@ print()
 # Expected output
 
 #Checking cmos area has correct offset in .img files
-#  Image # 0 PASS : Found [01 00 00 00]   [./DataSamples/SN33-Real.img]
+#  Image # 0 PASS : Found [01 00 00 00 98 ba dc fe] @ 0x75be663 [./DataSamples/SN33-Real.img]
 #
 #Checksum from image : ea 75 09 e9
 #
